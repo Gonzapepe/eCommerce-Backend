b419650607db33c2ecfe94f196ad8635e7b40de0
Gonzapepe <addamogonzalo@gmail.com>
2021-09-28 03:06:19 -0300

    bugfix: read JWT_TOKEN as string

--- src/utils/createJwtToken.ts
+++ src/utils/createJwtToken.ts
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import { JwtPayload } from "../types/JwtPayload";
 
 export const createJwtToken = (payload: JwtPayload): string => {
-  return jwt.sign(payload, process.env.JWT_SECRET!, {
+  return jwt.sign(payload, process.env.JWT_SECRET as string, {
     expiresIn: process.env.JWT_EXPIRATION,
   });
 };
