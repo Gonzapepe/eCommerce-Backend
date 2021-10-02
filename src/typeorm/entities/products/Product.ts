0f6ef945aa850669ea536cd436981830c710885f
Gonzapepe <addamogonzalo@gmail.com>
2021-10-02 04:08:00 -0300

    feat: created products entities

---
+++ src/typeorm/entities/products/Product.ts
@@ -0,0 +1,30 @@
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+  JoinColumn,
+  Entity,
+} from "typeorm";
+
+@Entity()
+export class Product extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column()
+  title!: string;
+
+  @Column("int", { default: 0 })
+  stock!: number;
+
+  @Column("decimal", { precision: 5, scale: 2 })
+  price!: number;
+
+  @Column("int", { default: 0 })
+  features!: number;
+
+  @Column({ default: "Este producto no tiene descripción." })
+  description!: string;
+}
