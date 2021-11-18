a8b5e867a2606526a163217c607df88fc197f9b7
Gonzapepe <addamogonzalo@gmail.com>
2021-11-18 02:17:14 -0300

    test: added cartItem tests

--- test/cart/cart.test.ts
+++ test/cart/cart.test.ts
@@ -5,31 +5,36 @@ import { getRepository, Connection, Repository } from "typeorm";
 
 import { app } from "../../src";
 import { dbCreateConnection } from "../../src/typeorm/dbCreateConnection";
-import { Product } from "../../src/typeorm/entities/products/Product";
 import { Role } from "../../src/typeorm/entities/users/types";
 import { User } from "../../src/typeorm/entities/users/User";
+import { Cart } from "../../src/typeorm/entities/cart/Cart";
 
 describe("Cart and CartItems API", () => {
   let dbConnection: Connection;
   //   let productRepository: Repository<Product>;
   let userRepository: Repository<User>;
+  let cartRepository: Repository<Cart>;
 
   let userPassword = "pass1";
   let standardUserToken: string = "";
   const standardUser = new User();
+  const cart = new Cart();
 
   standardUser.name = "asdasd";
   standardUser.surname = "Mayhew";
   standardUser.email = "eqweqweqw@test.com";
   standardUser.document = 42884897;
   standardUser.phone = 911444569624;
+  standardUser.cart = cart;
   standardUser.password = userPassword;
   standardUser.hashPassword();
   standardUser.role = "STANDARD" as Role;
+  cart.user = standardUser;
 
   before(async () => {
     dbConnection = await dbCreateConnection();
     userRepository = getRepository(User);
+    cartRepository = getRepository(Cart);
   });
 
   after(async () => {
@@ -42,6 +47,7 @@ describe("Cart and CartItems API", () => {
 
   beforeEach(async () => {
     await userRepository.save(standardUser);
+    await cartRepository.save(cart);
     let res = await request(app)
       .post("/v1/auth/login")
       .send({ email: standardUser.email, password: userPassword });
@@ -57,7 +63,7 @@ describe("Cart and CartItems API", () => {
       let res = await request(app)
         .get("/v1/cart")
         .set({ Authorization: standardUserToken });
-
+      console.log("MENSAJEEE", res.body);
       expect(res.status).to.equal(200);
       expect(res.body.message).to.equal("Carrito encontrado");
     });
@@ -69,6 +75,14 @@ describe("Cart and CartItems API", () => {
         .post("/v1/products/6eeebba5-ce2e-4c90-9695-f35db2f9bc59")
         .set({ Authorization: standardUserToken })
         .send({ quantity: 1 });
+
+      expect(res.status).to.equal(200);
+      expect(res.body.message).to.equal(
+        "Producto añadido al carrito satisfactoriamente"
+      );
+      expect(res.body.data.product.title).to.equal(
+        "Pintura especial albatex rojo marinado"
+      );
     });
   });
 });
