76bdb895422f916608915bc3f5025c0767d7a49c
Gonzapepe <addamogonzalo@gmail.com>
2021-12-09 18:13:35 -0300

    Added multer for file uploading

---
+++ src/controllers/images/index.ts
@@ -0,0 +1,30 @@
+import multer from "multer";
+
+import { Request } from "express";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const fileFilter = (req: Request, file: any, cb: any) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  },
+  fileFilter: fileFilter,
+});
+
+export const fileUpload = upload.single("file");
