9feaf6779895419403d93ee8f3d8c0dea19e89d0
Gonzapepe <addamogonzalo@gmail.com>
2021-12-18 21:32:52 -0300

    feat: added list query

--- src/controllers/subcategory/list.ts
+++ src/controllers/subcategory/list.ts
@@ -1,32 +1,64 @@
 import { Request, Response, NextFunction } from "express";
 
-import { getRepository } from "typeorm";
+import { createQueryBuilder, getRepository } from "typeorm";
 import { CustomError } from "../../utils/response/custom-error/CustomError";
 import { Subcategory } from "../../typeorm/entities/categories/Subcategory";
+import { Product } from "../../typeorm/entities/products/Product";
 
 export const list = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const subcategories = await getRepository(Subcategory)
-      .createQueryBuilder("subcategory")
-      .leftJoinAndSelect("subcategory.products", "subcategories")
-      .addSelect("subcategory.countProducts()", "count")
-      .getMany();
+  const { name } = req.query;
 
-    if (!subcategories) {
-      const customError = new CustomError(
-        404,
-        "General",
-        "Subcategoría no encontradas",
-        ["No encontrado"]
-      );
+  if (name) {
+    const names = name.toString().split(",");
+    console.log("NOMBRES: ", names);
+
+    const products: Product[] = [];
+
+    names.forEach(async (product) => {
+      const response = await getRepository(Product)
+        .createQueryBuilder("product")
+        .innerJoin("product.subcategories", "subcategory")
+        .where("subcategory.name = :name", { name: product })
+        .getMany();
+      products.push(...response);
+      console.log("RESPUESTA: ", response);
+      console.log("PRODUCTS DENTRO DE FOREACH: ", products);
+    });
+
+    // for(const x in names) {
+    //   const response = await getRepository(Product)
+    //       .createQueryBuilder("product")
+    //       .innerJoin("product.subcategories", "subcategory")
+    //       .where("subcategory.name = :name", { name: names[x] })
+    //       .getMany();
+    //       products.push(...response)
+    // }
+
+    console.log("PRODUCTOS: ", products);
+    res.customSuccess(200, "Productos: ", products);
+  } else {
+    try {
+      const subcategories = await getRepository(Subcategory)
+        .createQueryBuilder("subcategory")
+        .leftJoinAndSelect("subcategory.products", "subcategories")
+        .getMany();
+
+      if (!subcategories) {
+        const customError = new CustomError(
+          404,
+          "General",
+          "Subcategoría no encontradas",
+          ["No encontrado"]
+        );
+
+        return next(customError);
+      }
+
+      res.customSuccess(200, "Subcategorías: ", subcategories);
+    } catch (err) {
+      const customError = new CustomError(400, "Raw", "Error", null, err);
 
       return next(customError);
     }
-
-    res.customSuccess(200, "Subcategorías: ", subcategories);
-  } catch (err) {
-    const customError = new CustomError(400, "Raw", "Error", null, err);
-
-    return next(customError);
   }
 };
