42bfd6e00c79c45c91777e5cf8bffb3dbb2645d2
Gonzapepe <addamogonzalo@gmail.com>
2022-01-26 21:40:49 -0300

    Payment Working fine

--- src/controllers/cart/feedback.ts
+++ src/controllers/cart/feedback.ts
@@ -2,8 +2,10 @@ import { Request, Response, NextFunction } from "express";
 import mercadopago from "mercadopago";
 import { CustomError } from "../../utils/response/custom-error/CustomError";
 import { User } from "../../typeorm/entities/users/User";
-import { getConnection } from "typeorm";
+import { Cart } from "../../typeorm/entities/cart/Cart";
+import { getConnection, getRepository } from "typeorm";
 import { CartItem } from "../../typeorm/entities/cart/CartItems";
+import { Product } from "../../typeorm/entities/products/Product";
 
 export const feedback = async (
   req: Request,
@@ -25,52 +27,56 @@ export const feedback = async (
       return next(customError);
     }
 
-    console.log("USUARIO: ", user);
+    const cart = await getRepository(Cart)
+      .createQueryBuilder("cart")
+      .leftJoinAndSelect("cart.cartItems", "items")
+      .where("cart.id = :id", { id: user.cartId })
+      .getOne();
 
     // Para hacer: 2 transacciones, la primera que elimine la cantidad de stock que tenia el cliente en el carrito en los determinados productos,
     // La segunda transaccion, que elimine el total del carrito del cliente
+    getConnection().transaction(async (tm) => {
+      await tm.query(
+        `
+        update cart
+        set total = 0
+        where "id" = $1
+      `,
+        [user.cartId]
+      );
+    });
 
-    try {
-      await getConnection().transaction(async (tm) => {
-        //resetear el total a 0
-        await tm.query(
-          `
-          update cart 
-          set total = 0
-          where "id" = $1
-        `,
-          [user.cartId]
-        );
+    cart.cartItems.forEach(async (item) => {
+      const response = await getRepository(CartItem)
+        .createQueryBuilder("cartItem")
+        .leftJoinAndSelect("cartItem.product", "items")
+        .where("cartItem.id = :id", { id: item.id })
+        .getOne();
+      console.log("CART ITEM Y RELACIONES: ", response);
+      const product = await Product.findOne({
+        where: { id: response.product.id },
       });
+      product.stock = product.stock - item.quantity;
 
-      // Resetea el Array de cartItems donde el Id sea igual que el id del carrito
-      await getConnection()
-        .createQueryBuilder()
-        .delete()
-        .from(CartItem)
-        .where("cartId = :id", { id: user.cartId })
-        .execute();
-
-      console.log("USUARIO DESPUES DE LA QUERY: ", user.cart.total);
-    } catch (err) {
-      const customError = new CustomError(400, "Raw", "Error", null, err);
-      return next(customError);
-    }
+      await Product.save(product);
+      console.log("PRODUCTO: ", product);
+    });
 
     // Eliminar el item del carrito
-    // const deletedCartItems = getConnection().createQueryBuilder().delete().from(CartItem).where('cartItem.cartid = :id', {id: user.cartId}).execute()
+    getConnection()
+      .createQueryBuilder()
+      .delete()
+      .from(CartItem)
+      .where("cartItem.cartid = :id", { id: user.cartId })
+      .execute();
 
     const payment = await mercadopago.payment.findById(
       Number(req.query.payment_id)
     );
-    const merchantOrder = await mercadopago.merchant_orders.findById(
-      payment.body.order.id
-    );
-    const preferenceId = merchantOrder.body.preference_id;
+
     const status = payment.body.status;
-    if (status === "approved") {
-    }
-    console.log("PREFERENCE ID: ", preferenceId);
+    // if (status === "approved") {
+    // }
     res.customSuccess(200, "Pago acreditado", {
       Payment: req.query.payment_id,
       Status: req.query.status,
